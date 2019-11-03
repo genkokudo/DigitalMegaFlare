27c019c1521bc2fa448515b1428d9e7be490bdc0
ginpaydo <y-toyoda@live.jp>
2019-11-04 00:16:37 +0900

    ページをいくつか作成

---
+++ DigitalMegaFlare/Pages/Doodle/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DigitalMegaFlare.Pages.Doodle
+{
+    /// <summary>
+    /// 落書き帳
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
--- DigitalMegaFlare/Pages/SimpleGenerate/Index.cshtml.cs
+++ DigitalMegaFlare/Pages/SimpleGenerate/Index.cshtml.cs
@@ -8,22 +8,22 @@ using Microsoft.EntityFrameworkCore;
 using DigitalMegaFlare.Data;
 using DigitalMegaFlare.Models;
 
-namespace DigitalMegaFlare.Pages.TestPages
+namespace DigitalMegaFlare.Pages.SimpleGenerate
 {
     public class IndexModel : PageModel
     {
-        private readonly DigitalMegaFlare.Data.ApplicationDbContext _context;
+        //private readonly DigitalMegaFlare.Data.ApplicationDbContext _context;
 
-        public IndexModel(DigitalMegaFlare.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+        //public IndexModel(DigitalMegaFlare.Data.ApplicationDbContext context)
+        //{
+        //    _context = context;
+        //}
 
         public IList<TestData> TestData { get;set; }
 
         public async Task OnGetAsync()
         {
-            TestData = await _context.TestDatas.ToListAsync();
+            //TestData = await _context.TestDatas.ToListAsync();
         }
     }
 }
