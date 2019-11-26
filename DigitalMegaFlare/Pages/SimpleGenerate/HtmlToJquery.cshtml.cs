2ba2ccba83c1476426c7865938f0009746a24eba
ginpaydo <y-toyoda@live.jp>
2019-11-26 20:51:30 +0900

    プロジェクト参照を変更

--- DigitalMegaFlare/Pages/SimpleGenerate/HtmlToJquery.cshtml.cs
+++ DigitalMegaFlare/Pages/SimpleGenerate/HtmlToJquery.cshtml.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using HtmlToDom;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mintea.HtmlToDom;
 
 namespace DigitalMegaFlare.Pages.SimpleGenerate
 {
