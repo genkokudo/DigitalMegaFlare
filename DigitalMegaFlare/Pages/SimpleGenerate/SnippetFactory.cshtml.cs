31f55a06ed2650d3a50bb114c1747d8c2b12d3f4
ginpaydo <y-toyoda@live.jp>
2019-11-27 00:12:14 +0900

    後は項目追加していくだけ！

--- DigitalMegaFlare/Pages/SimpleGenerate/SnippetFactory.cshtml.cs
+++ DigitalMegaFlare/Pages/SimpleGenerate/SnippetFactory.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DigitalMegaFlare.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,12 +11,26 @@ namespace DigitalMegaFlare.Pages.SimpleGenerate
     public class SnippetFactoryModel : PageModel
     {
         [BindProperty]
-        public string Input { get; set; }
+        public Snippet Input { get; set; }
+
         [BindProperty]
         public string Output { get; set; }
-        public void OnGet()
-        {
 
+        public IActionResult OnGetAsync()
+        {
+            // TODO:クッキーから読めないかなあー
+            Input = new Snippet();
+            return Page();
+        }
+
+        public IActionResult OnPostGenerateXmlAsync()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPostDownloadAsync()
+        {
+            return Page();
         }
     }
 }
