ee6ef05fdb35ba3bd82cc7a08f7c61db5080a21d
toyoda <toyoda@divakk.co.jp>
2019-12-04 11:01:18 +0900

    コントローラ追加

---
+++ DigitalMegaFlare/Controllers/DoodleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalMegaFlare.Controllers
+{
+    /// <summary>
+    /// テスト実装のコントローラーです。
+    /// </summary>
+    //[Authorize]
+    public class DoodleController : Controller
+    {
+
+        public ActionResult Download(long id)
+        {
+            return Ok();
+            //return File(new byte[], "application/pdf", $"stampHistory{id}.pdf");
+        }
+    }
+}
