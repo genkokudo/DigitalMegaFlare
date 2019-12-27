fd060ad95b401e1dfc329fc1b9017ddf7070b5ba
ginpaydo <y-toyoda@live.jp>
2019-12-28 04:50:20 +0900

    Reactを外部ファイルに出す

---
+++ DigitalMegaFlare/wwwroot/js/reactDoodle.js
@@ -0,0 +1,53 @@
+﻿
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: 'Hello Work!'
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p> {this.state.text} </p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, $('#doodle').get(0));
+
+
+class HelloWork extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: 'Hello Work!'
+        }
+    }
+
+    render() {
+        //// 
+        //return (
+        //    <div>
+        //        Hello Work {this.props.name}
+        //    </div>
+        //)
+        // 動く
+        return <div>Hello Work {this.props.name}</div>;
+        //// 改行すると動かない（謎）
+        //return
+        //<div>Hello Work {this.props.name}</div>;
+    }
+}
+
+const hwElement = <HelloWork name="01" />;
+ReactDOM.render(hwElement, $('#app01').get(0));
+
+const hwFactory = React.createFactory(HelloWork);
+ReactDOM.render(hwFactory({ name: '02' }), $('#app02').get(0));
+
+// 動作確認
+alert("正常です");
--- DigitalMegaFlare/wwwroot/js/site.js
+++ DigitalMegaFlare/wwwroot/js/site.js
@@ -2,24 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: 'Hello Work!'
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <p> {this.state.text} </p>
-            </div>
-        )
-    }
-}
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('doodle')
-);
