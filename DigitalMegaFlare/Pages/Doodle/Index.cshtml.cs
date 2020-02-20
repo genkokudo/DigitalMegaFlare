c378fcd304c262d7ea73078cc36a9eb0677266a5
ginpaydo <y-toyoda@live.jp>
2020-02-21 00:05:21 +0900

    取り敢えずRazorEngineが動いたから満足。

--- DigitalMegaFlare/Pages/Doodle/Index.cshtml.cs
+++ DigitalMegaFlare/Pages/Doodle/Index.cshtml.cs
@@ -8,7 +8,7 @@ using DigitalMegaFlare.Models;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+using RazorLight;
 
 namespace DigitalMegaFlare.Pages.Doodle
 {
@@ -29,10 +29,32 @@ namespace DigitalMegaFlare.Pages.Doodle
             return Page();
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
-            ViewData["Message"] = "ボタンを押しますたm9(･Å･)";
+            var engine = new RazorLightEngineBuilder()
+                          // required to have a default RazorLightProject type, but not required to create a template from string.
+                          .UseEmbeddedResourcesProject(typeof(Program))
+                          .UseMemoryCachingProvider()
+                          .Build();
+
+            var template = "Name: @Model.Name, Age: @Model.Age";
+            var model = new { Name = "hauhau", Age = 18 };
+
+            string result = await engine.CompileRenderStringAsync("templateKey", template, model);
+
+            ViewData["Message"] = result;
             return Page();
+
+//            ■匿名型の動的作成 
+            //dynamic tmp2 = new ExpandoObject(); 
+            //var dic = new Dictionary<string, object>() { { "name", "hogehoge" }, { "age", 10 } }; 
+ 
+            //IDictionary<string, object> wk = tmp2; 
+            //foreach (var item in dic) { wk.Add(item.Key, item.Value); } 
+ 
+            //Console.WriteLine("----- tmp2 -----"); 
+            //Console.WriteLine(tmp2.name); 
+            //Console.WriteLine(tmp2.age); 
         }
     }
 
