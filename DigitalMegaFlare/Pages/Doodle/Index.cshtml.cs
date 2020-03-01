4345b71d143205f67f8d9cd92dea286ca48613c8
ginpaydo <y-toyoda@live.jp>
2020-03-01 16:32:23 +0900

    リファクタリング

--- DigitalMegaFlare/Pages/Doodle/Index.cshtml.cs
+++ DigitalMegaFlare/Pages/Doodle/Index.cshtml.cs
@@ -1,31 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using ClosedXML.Excel;
-using DigitalMegaFlare.Infrastructure;
-using DigitalMegaFlare.Data;
-using DigitalMegaFlare.Models;
-using DigitalMegaFlare.Pages.SimpleGenerate.Razor;
-using MediatR;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.FileProviders;
-using Mintea.HtmlToDom;
-using RazorLight;
-using System.IO.Compression;
 using Mintea.RazorHelper;
-// TODO:Dictionaryを拡張して、存在したら追加、キーが存在しなかったらnew Listするメソッドと、new Dictionaryする拡張メソッドを作る。作れる？
+using RazorLight;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
 // "Doc"で始まるシートは読まない
 // "List"で終わるシートは読む
+// "Is"で始まるフィールドは、0,FALSE,False,-で始まる,空文字列はfalse、1,TRUE,Trueはtrue
+// "Settings.Index"が必須。
 // "RootList"の件数だけ生成する
-// TODO:"Project.Index"が必須、ということはProjectシート必須。->Settingsに変更する。
-// TODO:"Is"で始まるフィールドは、0,FALSE,False,-で始まる,空文字列はfalse、1,TRUE,Trueはtrue
-// TODO:各出力ファイル名を指定できるようにすること。将来的には複数種類出力のため各行出力したいが、今はProjectにRazorテンプレで書く。
+
+// TODO:各出力ファイル名を指定できるようにすること。将来的には複数種類出力のため各行出力したいが。
+// 現在は、RootListと、その添字をmodelに入れて送ってる感じ。Razor中でRootList[index]で生成している。
 namespace DigitalMegaFlare.Pages.Doodle
 {
     /// <summary>
@@ -88,17 +79,25 @@ namespace DigitalMegaFlare.Pages.Doodle
                     var excel = RazorHelper.ReadExcel(excelDirectry, excelName, true);
 
                     // Modelの作成
-                    var model = RazorHelper.CreateModel(excel);
+                    dynamic model = null ;
+                    try
+                    {
+                        model = RazorHelper.CreateModel(excel);
+                    }
+                    catch (Exception e)
+                    {
+                        ViewData["Error"] = e.Message;
+                        return Page();
+                    }
 
-
-                    // 生成
+                    // ソース生成
                     string result = "";
                     var outFileList = new List<string>();
                     // ↓この"RootList"は動的に変えられないので、ファイル生成の一覧となるListシートの名前は"RootList"固定にする。
                     for (int i = 0; i < model.RootList.Count; i++)
                     {
-                        // 変数入れれるかな？
-                        model.Project.Index = i.ToString();
+                        // 変数入れる
+                        model.Settings.Index = i.ToString();
 
                         // 同じキーを指定すると登録したスクリプトを使いまわすことが出来るが、何故か2回目以降Unicodeにされるので毎回違うキーを使う。
                         result = await engine.CompileRenderStringAsync($"{model.RootList[i].Name}", template, model);
