c72cb58579a735e60d8566c23615815d499a5ba7
ginpaydo <y-toyoda@live.jp>
2020-03-14 22:53:41 +0900

    ファイルの取得

---
+++ DigitalMegaFlare/Controllers/RazorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalMegaFlare.Controllers
+{
+    /// <summary>
+    /// Razor実験室のコントローラーです。
+    /// </summary>
+    //[Authorize]
+    public class RazorController : Controller
+    {
+
+        public ActionResult Load(string filepath)
+        {
+            var data = System.IO.File.ReadAllText(filepath);
+            return Ok(new { Message = $"{data}" });
+        }
+
+        // using Microsoft.AspNetCore.Mvc
+        public ActionResult Test(IFormFile file)
+        {
+            return Ok();
+            //return File(new byte[], "application/pdf", $"stampHistory{id}.pdf");
+        }
+    }
+}
